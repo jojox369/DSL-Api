acfbbe2f54c7dc66b07deb4e8cfc4f297f69f2c8
João Vitor Santos <joaooviitorr@hotmail.com>
2020-11-15 14:29:17 -0300

    Mudança de banco de dados

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,12 @@
 const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 
 module.exports = {
-  type: 'sqlite',
+  type: 'mysql',
   host: process.env.MYSQL_HOST,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: rootDir + '/database/database.sqlite',
+  //database: rootDir + '/database/database.sqlite',
+  database: process.env.MYSQL_DATABASE,
   synchronize: true,
   entities: [rootDir + '/models/*.{js,ts}'],
 
