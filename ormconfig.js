9493c2e1850604da01afa01dee78de714d5db253
João Vitor Santos <joaooviitorr@hotmail.com>
2020-11-17 00:53:29 -0300

    Atualização na configuração;

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,7 @@ module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   host: process.env.MYSQL_HOST,
+  port: 3306,
   //database: rootDir + '/database/database.sqlite',
   database: process.env.MYSQL_DATABASE,
   synchronize: true,
