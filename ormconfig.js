3bc665f1d9d1e42feeeee9d7d5ed4f19bc0fe28e
João Vitor Santos <joaooviitorr@hotmail.com>
2020-11-17 00:41:45 -0300

    Mudança bando de dados;

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,12 @@
 const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 
 module.exports = {
-  type: 'sqlite',
-
-  database: rootDir + '/database/database.sqlite',
+  type: 'mysql',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  //database: rootDir + '/database/database.sqlite',
+  database: process.env.MYSQL_DATABASE,
   synchronize: true,
   entities: [rootDir + '/models/*.{js,ts}'],
 
