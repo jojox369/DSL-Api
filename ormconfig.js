605e9ca9365bd0599e755b88a48138fe2e567152
João Vitor Santos <joaooviitorr@hotmail.com>
2020-11-17 21:08:31 -0300

    Teste mysql;

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,14 @@
 const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 
 module.exports = {
-  type: 'sqlite',
-  /* username: process.env.MYSQL_USER,
+  type: 'mysql',
+  username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   host: process.env.MYSQL_HOST,
   port: 3306,
-  database: process.env.MYSQL_DATABASE, */
+  database: process.env.MYSQL_DATABASE,
 
-  database: rootDir + '/database/database.sqlite',
+  //database: rootDir + '/database/database.sqlite',
   synchronize: true,
 
   entities: [rootDir + '/models/*.{js,ts}'],
